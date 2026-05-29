111d934aaeaaef85ef54416445658c25c7d7c4bb
Kristi Lim <klim@ebsco.com>
2026-05-29 09:59:01 -0500

    feat: lesson that uses the console colors in the cli app

--- dart-tutorial/dartpedia/command_runner/lib/src/command_runner_base.dart
+++ dart-tutorial/dartpedia/command_runner/lib/src/command_runner_base.dart
@@ -6,20 +6,30 @@ import 'arguments.dart';
 import 'exceptions.dart';
 
 class CommandRunner {
-  CommandRunner({this.onError});
+  CommandRunner({this.onOutput, this.onError});
 
   final Map<String, Command> _commands = <String, Command>{};
 
   UnmodifiableSetView<Command> get commands =>
       UnmodifiableSetView<Command>(<Command>{..._commands.values});
+
+  /// If not null, this method is used to handle output. Useful if you want to
+  /// execute code before the output is printed to the console, or if you
+  /// want to do something other than print output the console.
+  /// If null, the onInput method will [print] the output.
+  FutureOr<void> Function(String)? onOutput;
   FutureOr<void> Function(Object)? onError;
 
-  Future<void> run(List<String> input) async {
+    Future<void> run(List<String> input) async {
     try {
       final ArgResults results = parse(input);
       if (results.command != null) {
         Object? output = await results.command!.run(results);
-        print(output.toString());
+        if (onOutput != null) {
+          await onOutput!(output.toString());
+        } else {
+          print(output.toString());
+        }
       }
     } on Exception catch (exception) {
       if (onError != null) {
@@ -29,6 +39,7 @@ class CommandRunner {
       }
     }
   }
+
 
   void addCommand(Command command) {
     // TODO: handle error (Commands can't have names that conflict)
